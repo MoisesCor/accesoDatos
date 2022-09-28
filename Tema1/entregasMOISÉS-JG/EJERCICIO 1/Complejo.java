9997e412faa899109e9300fe114316da8a106189
MoisesCor <99491213+MoisesCor@users.noreply.github.com>
2022-09-28 19:54:30 +0200

    Update Complejo.java

--- Tema1/entregasMOISÉS-JG/EJERCICIO 1/Complejo.java
+++ Tema1/entregasMOISÉS-JG/EJERCICIO 1/Complejo.java
@@ -26,20 +26,22 @@ public class Complejo{
        
     }
 
-    public void sumar(Complejo c2) {
+    public Complejo sumar(Complejo c2) {
         //variables usadas para la suma
         double dSumaReal;
         double dSumaImag;
 
         //suma de los atributos correspondiente de ambos objetos
-        dSumaReal = c2.dPReal + dPReal; 
-        dSumaImag = c2.dPImag+ dPImag; 
+        dSumaReal = c2.dPReal + this.dPReal; 
+        dSumaImag = c2.dPImag+ this.dPImag; 
+        Complejo complejoResult = new Complejo(dSumaReal, dSumaImag);
 
         //Resultado con formáto por pantalla
         System.out.println("La parte REAL: ("+dPReal+","+c2.dPReal+") = "+dSumaReal);
         System.out.println("La parte IMAGINARIA: ("+dPImag+","+c2.dPImag+") = "+dSumaImag);
-        System.out.printf("(%.0f + %.0fi)",dSumaReal , dSumaImag); //Se usa para dar formato por pantalla muy útil
+       // System.out.printf("(%.0f + %.0fi)",dSumaReal , dSumaImag); //Se usa para dar formato por pantalla muy útil
         
+        return complejoResult;
     }
     /* Getters and Setters */
     public double getdPReal() {
@@ -56,8 +58,14 @@ public class Complejo{
 
     public void setdPImag(double dPImag) {
         this.dPImag = dPImag;
+    }
+
+    @Override
+    public String toString() {
+        return "Complejo [dPImag=" + dPImag + ", dPReal=" + dPReal + "]";
     };
  
 
     }
+ 
  
