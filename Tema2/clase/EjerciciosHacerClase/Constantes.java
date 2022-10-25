8540bac565420395d8c7bd032ba4dbcc4d5ee992
MoisesCor <99491213+MoisesCor@users.noreply.github.com>
2022-10-25 12:36:13 +0200

    Delete Tema2 directory

--- Tema2/clase/EjerciciosHacerClase/Constantes.java
+++
@@ -1,6 +0,0 @@
-package TEMA2.main.clase.EjerciciosHacerClase;
-
-public class Constantes {
-    public final static  String RUTA ="C:\\Users\\Moisés\\Desktop\\DAM 2\\Acceso a datos\\TEMA2\\";
-    public final static  String RUTA2 ="C:\\Moises\\ppp\\";
-}
